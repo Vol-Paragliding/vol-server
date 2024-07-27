08d357d4df74518693f26f98f7ec54f12456a37d
Zachery Converse <zacheryconverse@gmail.com>
2024-07-27 12:04:29 -0600

    feat: set db dir based on node env

--- db.js
+++ db.js
@@ -3,9 +3,14 @@ const mkdirp = require("mkdirp");
 const crypto = require("crypto");
 require("dotenv").config();
 
-mkdirp.sync("./var/db");
+const dbPath =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_DATABASE_URL
+    : process.env.DEV_DATABASE_URL;
 
-const db = new sqlite3.Database("./var/db/vol.db");
+mkdirp.sync(require("path").dirname(dbPath));
+
+const db = new sqlite3.Database(dbPath);
 
 const createTable = () => {
   db.run(
