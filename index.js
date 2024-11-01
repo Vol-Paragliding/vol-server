9722413c66f874ed5035089c902a7d7fceb63cb8
Zachery Converse <zacheryconverse@gmail.com>
2024-11-01 09:57:50 -0600

    feat: redirect to new domain

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  if (
+    req.hostname === "volflights.com" ||
+    req.hostname === "www.volflights.com"
+  ) {
+    return res.redirect(301, `https://vol.flights${req.originalUrl}`);
+  }
+  next();
+});
+
 app.use(cors());
-app.use(
-  cors({
-    origin: ["https://volflights.com", "http://localhost:3000"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://volflights.com",
+//       "https://vol.flights",
+//       "http://localhost:3000",
+//     ],
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -36,4 +50,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", authRoutes);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
