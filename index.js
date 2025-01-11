c357875c7d93a9ef8b1cb0b5eb1b884ed5e2de01
Zachery Converse <zacheryconverse@gmail.com>
2025-01-10 18:07:50 -0700

    chore: dev on mobile device

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://192.168.86.26:3000", // Replace with your computer's IP
+//       "http://localhost:3000",
+//     ],
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true, // Allow cookies and authentication headers
+//   })
+// );
 // app.use(
 //   cors({
 //     origin: [
@@ -41,4 +51,6 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(8080, "0.0.0.0", () => console.log(`Server running on port 8080`));
+// app.listen(8080, '192.168.68.26', () => console.log(`Server running on port 8080`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
